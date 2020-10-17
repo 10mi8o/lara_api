09b14c916ddbdfb0f4f11fc37c3ed0d6ef657387
10mi8o <totoma855@gmail.com>
2020-10-18 08:57:28 +0900

    ArticlesのAPI定義

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
@@ -13,7 +15,8 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        return $articles;
     }
 
     /**
@@ -34,7 +37,11 @@ class ArticlesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $article = new Article;
+        $article->title = $request->body;
+        $article->body = $request->body;
+        $article->save();
+        return redirect('api/articles');
     }
 
     /**
@@ -45,7 +52,8 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        //
+        $article = Article::find($id);
+        return $article;
     }
 
     /**
@@ -68,7 +76,11 @@ class ArticlesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+        return redirect("api/articles/".$id);
     }
 
     /**
@@ -79,6 +91,8 @@ class ArticlesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $article = Article::find($id);
+        $article->delete();
+        return redirect('api/articles');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::group(['middleware' => ['api']], function() {
-//     Route::resource(articles, 'Api\ArticlesController');
-// });
+Route::group(['middleware' => ['api']], function(){
+    Route::resource('articles', 'Api\ArticlesController');
+});
