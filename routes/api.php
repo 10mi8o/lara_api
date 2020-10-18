05f792c18d989ce80f0fcd6aa75c38f2ffd2bfef
10mi8o <totoma855@gmail.com>
2020-10-18 09:35:21 +0900

    API叩くためにCors設定追加

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['api']], function(){
+Route::group(['middleware' => ['api', 'cors']], function(){
     Route::resource('articles', 'Api\ArticlesController');
 });
